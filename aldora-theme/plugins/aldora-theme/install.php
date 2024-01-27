8b25e8f4a72d523399204515a91f0981052ac386
slawkens <slawkens@gmail.com>
2024-01-27 09:58:07 +0100

    update aldora-theme version required + menus refactor

--- aldora-theme/plugins/aldora-theme/install.php
+++ aldora-theme/plugins/aldora-theme/install.php
@@ -1,55 +1,11 @@
 <?php
+
+use MyAAC\Plugins;
+
 defined('MYAAC') or die('Direct access not allowed!');
 
 $template = 'aldora';
 
-require PLUGINS . $template . '-theme/themes/aldora/config.php';
+require PLUGINS . $template . '-theme/themes/' . $template . '/config.php';
 
-if (!$db->hasTable(TABLE_PREFIX . 'menu')) {
-	return;
-}
-
-$query = $db->query('SELECT `id` FROM `' . TABLE_PREFIX . 'menu` WHERE `template` = ' . $db->quote($template) . ' LIMIT 1;');
-if ($query->rowCount() > 0) {
-	return;
-}
-
-$categories = require PLUGINS . $template . '-template/menus.php';
-
-foreach ($categories as $category => $menus) {
-	$i = 0;
-	foreach ($menus as $name => $link) {
-		$color = '';
-		$blank = 0;
-
-		if (is_array($link)) {
-			if (isset($link['color'])) {
-				$color = $link['color'];
-			}
-			if (isset($link['blank'])) {
-				$blank = $link['blank'] === true ? 1 : 0;
-			}
-
-			$link = $link['link'];
-		}
-
-		$insert_array = [
-			'template' => $template,
-			'name' => $name,
-			'link' => $link,
-			'category' => $category,
-			'ordering' => $i++,
-		];
-
-		// support for color and blank attributes since 0.8.0
-		if(version_compare(MYAAC_VERSION, '0.8.0', '>=')) {
-			$insert_array['blank'] = $blank;
-			$insert_array['color'] = $color;
-		}
-
-		$db->insert(TABLE_PREFIX . 'menu', $insert_array);
-
-		unset($color);
-	}
-
-}
+Plugins::installMenus($template, require PLUGINS . $template . '-theme/menus.php');
