5513e95baba5f77aa8a67a095fb615ed8cd46cd4
slawkens <slawkens@gmail.com>
2024-01-27 10:48:37 +0100

    Fix theme-aldora paths + install

--- theme-aldora/plugins/theme-aldora/install.php
+++ theme-aldora/plugins/theme-aldora/install.php
@@ -6,6 +6,6 @@ defined('MYAAC') or die('Direct access not allowed!');
 
 $template = 'aldora';
 
-require PLUGINS . $template . '-theme/themes/' . $template . '/config.php';
+require PLUGINS . "theme-$template/themes/$template/config.php";
 
-Plugins::installMenus($template, require PLUGINS . $template . '-theme/menus.php');
+Plugins::installMenus($template, require PLUGINS . "theme-$template/menus.php");
