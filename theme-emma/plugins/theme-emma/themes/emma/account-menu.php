b36269cb43c3044af1d4d52f2bdf5bab7a3fd105
slawkens <slawkens@gmail.com>
2025-10-21 11:11:36 +0200

    theme-emma: v2.1.1 - Fix menu to be shown on account/manage page

--- theme-emma/plugins/theme-emma/themes/emma/account-menu.php
+++ theme-emma/plugins/theme-emma/themes/emma/account-menu.php
@@ -1,7 +1,8 @@
 <?php
 defined('MYAAC') or die('Direct access not allowed!');
 
-global $action, $twig, $template_name;
-if($template_name == 'emma' && str_contains(PAGE, 'account/') && !str_contains(PAGE, 'account/manage')) {
+global $action, $twig, $template_name, $logged;
+if($logged && $template_name == 'emma' && str_contains(PAGE, 'account/')
+	&& !str_contains(PAGE, 'account/manage') && !str_contains(PAGE, 'account/create')) {
 	echo $twig->render('theme-emma/themes/emma/emma-account-menu.html.twig');
 }
