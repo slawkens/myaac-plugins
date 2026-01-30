d960a27d8d297257aab3b5ecfa74ea87295d5d9b
slawkens <slawkens@gmail.com>
2026-01-30 19:32:40 +0100

    password-strength: v2.3
    * Update for myaac 2.0 - support for change-password page

--- password-strength/plugins/password-strength/verify-strength.php
+++ password-strength/plugins/password-strength/verify-strength.php
@@ -1,4 +1,10 @@
 <?php
+defined('MYAAC') or die('Direct access not allowed!');
+
+/**
+ * @var OTS_Account $account_logged
+ * @var bool $logged
+ */
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -10,7 +16,7 @@ use ZxcvbnPhp\Zxcvbn;
 
 $userData = [];
 
-$postPassword = $_POST['new_password'] ?? '';
+$postPassword = $_POST['new_password'] ?? $_POST['password'] ?? '';
 if ($this->_type == HOOK_ACCOUNT_CREATE_POST) {
 	if (!setting('core.account_login_by_email')) {
 		$userData[] = $_POST['account'];
@@ -19,9 +25,20 @@ if ($this->_type == HOOK_ACCOUNT_CREATE_POST) {
 	if (setting('core.account_create_character_create') && isset($_POST['name'])) {
 		$userData[] = $_POST['name'];
 	}
-
-	$postPassword = $_POST['password'] ?? '';
 }
+
+if (isset($logged) && $logged) {
+	if (USE_ACCOUNT_NAME) {
+		$userData[] = $account_logged->getName();
+	}
+	else if (USE_ACCOUNT_NUMBER) {
+		$userData[] = $account_logged->getNumber();
+	}
+	else {
+		$userData[] = $account_logged->getId();
+	}
+}
+
 
 global $errors;
 
@@ -30,6 +47,7 @@ $strength = $zxcvbn->passwordStrength($postPassword, $userData);
 
 if (($strength['score'] + 1) < setting('password_strength.min_score')) {
 	$errors['password'] = 'Password is too weak. ' . $strength['feedback']['warning'];
+
+	global $passwordFailed;
+	$passwordFailed = true;
 }
-
-
